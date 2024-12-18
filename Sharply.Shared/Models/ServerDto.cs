d168e69d8c87a3781a5272b836d7a0f15cfb895c
Nathaniel Poppe <poppent@mail.uc.edu>
2024-12-17 22:25:19 -0500

    server owner

--- Sharply.Shared/Models/ServerDto.cs
+++ Sharply.Shared/Models/ServerDto.cs
@@ -4,6 +4,7 @@ public class ServerDto
 {
     public required int Id { get; set; }
     public required string Name { get; set; }
+    public required int OwnerId { get; set; }
     public List<ChannelDto> Channels { get; set; } = new List<ChannelDto>();
 
 }
